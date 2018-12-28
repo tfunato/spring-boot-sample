004b663e18bf59085764c2c4a88239974eb7804f
tfunato <tfunato@gmail.com>
2018-12-28 14:41:28 +0900

    initial commit

---
+++ src/main/java/jp/canetrash/spring/boot/sprintbootsample/SprintBootSampleApplication.java
@@ -0,0 +1,14 @@
+package jp.canetrash.spring.boot.sprintbootsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootSampleApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/jp/canetrash/spring/boot/sprintbootsample/SprintBootSampleApplicationTests.java
@@ -0,0 +1,17 @@
+package jp.canetrash.spring.boot.sprintbootsample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SprintBootSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
